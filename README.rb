9e7dd340add1939ecf1e6bdb6f1e8970dd1dfe7e
pocari <caffelattenonsugar@gmail.com>
2023-02-10 01:32:04 +0900

    パターン追加

--- README.rb
+++ README.rb
@@ -6,6 +6,9 @@
 - combination_use_stack.rb
   - 再帰を使わずにスタックを使って探索するパターン
 
+- min_route_use_queue.rb
+  - 最短経路問題を例に、キューを使って探査するパターン
+
 - combination_use_searcher.rb
   - main_use_stackと同様の検索だが、検索処理自体はフレームワークとして提供し、組み合わせの抽出に必要な状態の定義、次の状態の生成などを切り出したパターン
 
---
+++ min_route_use_queue.rb
@@ -0,0 +1,61 @@
+require 'set'
+require './queue'
+require './stack'
+
+def min_route(graph, start, goal)
+  results = []
+  data = Queue.new
+  visited = Set.new
+
+  data.push([
+    start,
+    nil,
+  ])
+  visited.add(start)
+
+  until data.empty?
+    state = data.pop
+    cur, _ = state
+    if cur == goal
+      return state
+    else
+      graph[cur].each do |next_node|
+        unless visited.include?(next_node)
+          data.push([
+            next_node,
+            state,
+          ])
+          visited.add(next_node)
+        end
+      end
+    end
+  end
+  false
+end
+
+# 下記のような経路の0〜7までの最短経路を探す
+#            0
+#           /|\
+#          / | \
+#         1--3--2
+#           / \
+#          5---4
+#           \  |
+#            6 |
+#             \|
+#              7
+# 上記のグラフを隣接リストで表現する
+graph = [
+  [1, 2, 3],       # 0
+  [0, 3],          # 1
+  [0, 3],          # 2
+  [0, 1, 2, 4, 5], # 3
+  [3, 5, 7],       # 4
+  [4, 6],          # 5
+  [5, 7],          # 6
+  [4, 6],          # 7
+]
+results = min_route(graph, 0, 7)
+if results
+  p results
+end
