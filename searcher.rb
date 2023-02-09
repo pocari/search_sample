9c4990239de3eeef7d758104a6e46c5702f43320
pocari <caffelattenonsugar@gmail.com>
2023-02-10 02:59:00 +0900

    コメント修正

--- searcher.rb
+++ searcher.rb
@@ -29,7 +29,7 @@ class Searcher
       if solved?(state)
         solved = true
         if handle_solved(state)
-          # ここでtrueを返すと、探索を「成功」でする
+          # ここでtrueを返すと、探索を「成功」で終了する
           return true
         end
       else
