65b0628e85c69c1e42cdee3ea5e0048ec9cdb2e9
pocari <caffelattenonsugar@gmail.com>
2023-02-25 14:11:46 +0900

    コメント追加

--- combination_recur.rb
+++ combination_recur.rb
@@ -3,9 +3,16 @@ def combination(ary, size, acc = [])
     [acc]
   else
     return [] if ary.empty?
+    # https://hiraocafe.com/note/combination.html
+    # の 組合わせの性質2 をつかう
+
+    # あるAをとり、残りのn-1(==ary[1..-1)個のからR-1個選ぶ(accにary[0]を追加するので残りR-1個しか選べない)場合
     use_first = combination(ary[1..-1], size, acc + [ary[0]])
+
+    # Aをとらず、残りのn-1(==ary[1..-1)個のからR個選ぶ(accがそのままなので残りR個選べる)場合
     unuse_first = combination(ary[1..-1], size, acc)
 
+    # その和
     use_first + unuse_first
   end
 end
