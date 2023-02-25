ce525e39afdb51286e6dd1662e889ec99c103ce3
pocari <caffelattenonsugar@gmail.com>
2023-02-25 13:55:40 +0900

    kukuのstack版がなかったので追加

---
+++ kuku_use_stack.rb
@@ -0,0 +1,23 @@
+require './stack'
+
+def kuku_use_stack
+  results = []
+  data = Stack.new
+
+  data.push([])
+
+  until data.empty?
+    acc = data.pop
+    if acc.size == 2
+      results << acc
+    else
+      (1..9).each do |i|
+        data.push(acc + [i])
+      end
+    end
+  end
+  results
+end
+
+results = kuku_use_stack
+p results
