66ad726e0365e03d5b2fbeefb5738e993f6a93ec
pocari <caffelattenonsugar@gmail.com>
2023-02-25 14:34:11 +0900

    コメント追加

--- combination_recur.rb
+++ combination_recur.rb
@@ -6,7 +6,7 @@ def combination(ary, size, acc = [])
     # https://hiraocafe.com/note/combination.html
     # の 組合わせの性質2 をつかう
 
-    # あるAをとり、残りのn-1(==ary[1..-1)個のからR-1個選ぶ(accにary[0]を追加するので残りR-1個しか選べない)場合
+    # あるA(ここでは先頭のary[0])をとり、残りのn-1(==ary[1..-1)個のからR-1個選ぶ(accにary[0]を追加するので残りR-1個しか選べない)場合
     use_first = combination(ary[1..-1], size, acc + [ary[0]])
 
     # Aをとらず、残りのn-1(==ary[1..-1)個のからR個選ぶ(accがそのままなので残りR個選べる)場合
